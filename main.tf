f0546aceb1846f2e52793f331b970807fa54a017
rknoplock <rafaelstp8@gmail.com>
2025-05-19 19:10:50 -0300

    Subindo projeto

--- main.tf
+++ main.tf
@@ -3,11 +3,12 @@ provider "aws" {
 }
 
 module "s3_bucket" {
-  source = "git::https://github.com/rknoplock/module-terraform-s3.git//?ref=v1.0.0"
-
+  source      = "git::https://github.com/rknoplock/module-terraform-s3.git//?ref=v1.0.1"
   bucket_name = "meu-bucket-via-modulo"
+  aws_region  = "us-east-1"
   tags = {
-    Environment = "rknoplock"
-    Owner       = "Equipe SRE"
+    Environment = "Prod"
+    Owner       = "infra"
   }
 }
+
